7aadf0c42469f407ae88003bd77cc04f5de679a9
Varun <varun.nagrare@gmail.com>
2020-07-25 11:44:38 +0530

    ADDED: Twitter Credentials file

---
+++ twitterConfig.py
@@ -0,0 +1,7 @@
+# These information comes from Twitter API
+# Create a Twitter Account and Get These Information from apps.twitter.com
+
+API_Key = 'API_KEY'
+API_SecretKey = 'API_SECRET_KEY'
+Access_Token = 'ACCESS_TOKEN'
+Access_SecretToken = 'ACCESS_SECRET_TOKEN'
